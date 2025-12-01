727dd7eada5b624b7463e16ff7b1478833389ac6
tajimayuki <softte2su77@gmail.com>
2025-12-01 23:00:35 +0900

    レスポンシブ対応のため viewport メタタグを追加

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'アプリ名')</title>
 
     @vite('resources/css/app.css')
