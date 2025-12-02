8521f63a1098f9cdcb09b36ca0fbea9ccc917e5b
tajimayuki <softte2su77@gmail.com>
2025-12-02 23:24:38 +0900

    ヘッダーを共通化

---
+++ resources/views/components/site-header.blade.php
@@ -0,0 +1,28 @@
+<header class="site-header" role="banner">
+    <div class="site-header__left">
+        <a class="site-header__brand" href="/">
+            <span class="site-header__logo">ロゴ</span>
+        </a>
+    </div>
+
+    <div class="site-header__center">
+        <form class="search search--header" action="" method="GET" role="search">
+            <input class="search__input" type="search" name="q" placeholder="検索" aria-label="検索" />
+        </form>
+    </div>
+
+    <button class="site-header__menu-btn" type="button" aria-label="メニュー">☰</button>
+
+    <div class="site-header__right">
+        <nav class="site-header__nav" aria-label="メインナビゲーション">
+            <a class="site-header__nav-link" href="#">ホーム</a>
+            <a class="site-header__nav-link" href="#">通知</a>
+            <a class="site-header__nav-link" href="#">自分</a>
+        </nav>
+
+        <form class="site-header__logout" action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button type="submit" class="button button--outline site-header__logout-btn">ログアウト</button>
+        </form>
+    </div>
+</header>
--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -7,34 +7,7 @@
 @endsection
 
 @section('content')
-  <div class="timeline">
-    <header class="timeline__header" role="banner">
-        <div class="timeline__header-left">
-            <a class="timeline__brand" href="/">
-                <span class="timeline__logo">ロゴ</span>
-            </a>
-        </div>
-
-        <div class="timeline__header-center">
-            <form class="search search--header" action="" method="GET" role="search">
-                <input class="search__input" type="search" name="q" placeholder="検索" aria-label="検索" />
-            </form>
-        </div>
-
-        <button class="timeline__menu-btn" type="button" aria-label="メニュー">☰</button>
-
-        <div class="timeline__header-right">
-            <nav class="timeline__nav" aria-label="メインナビゲーション">
-                <a class="timeline__nav-link" href="#">ホーム</a>
-                <a class="timeline__nav-link" href="#">通知</a>
-                <a class="timeline__nav-link" href="#">自分</a>
-            </nav>
-
-            <form class="timeline__logout" action="{{ route('logout') }}" method="POST">
-                @csrf
-                <button type="submit" class="button button--outline timeline__logout-btn">ログアウト</button>
-            </form>
-        </div>
-    </header>
-  </div>
+    <div class="timeline">
+        @include('components.site-header')
+    </div>
 @endsection
