9a0d4cf1f6abfe6a43699ce683b5f7ae9e935dd7
tajimayuki <softte2su77@gmail.com>
2025-12-29 00:20:36 +0900

    クラス名修正

--- tests/Feature/LikeControllerTest.php
+++ tests/Feature/LikeControllerTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
-class LikeController extends TestCase
+class LikeControllerTest extends TestCase
 {
     use RefreshDatabase;
 
