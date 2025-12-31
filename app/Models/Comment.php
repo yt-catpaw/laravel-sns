647681b6cc6cba4a27c29d8a77d91fc0887aff57
tajimayuki <softte2su77@gmail.com>
2025-12-31 15:24:14 +0900

    モデルリレーション追加

---
+++ app/Models/Comment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $post_id
+ * @property int $user_id
+ * @property int|null $parent_id
+ * @property string $body
+ * @method static \Database\Factories\CommentFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|\App\Models\Comment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|\App\Models\Comment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|\App\Models\Comment query()
+ * @mixin \Eloquent
+ */
+class Comment extends Model
+{
+    /** @use HasFactory<\Database\Factories\CommentFactory> */
+    use HasFactory;
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
