4c5ed21bac342777af8e2483effe2cf1eb958b04
tajimayuki <softte2su77@gmail.com>
2025-12-31 15:50:06 +0900

    対象カラムを保存できるように指定

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -25,6 +25,12 @@ class Comment extends Model
     use HasFactory;
 
 
+    protected $fillable = [
+        'post_id',
+        'user_id',
+        'body',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
