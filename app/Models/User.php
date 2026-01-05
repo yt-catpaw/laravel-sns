bd8d1e2447db88a502c7f08806605854e9a924f4
tajimayuki <softte2su77@gmail.com>
2026-01-05 19:18:14 +0900

    リレーション追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,8 +17,11 @@ use Illuminate\Notifications\Notifiable;
  * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Comment> $comments
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Post> $likedPosts
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\PostView> $postViews
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Post> $posts
  * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder<static>|\App\Models\User newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|\App\Models\User newQuery()
@@ -79,5 +82,10 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Post::class, 'likes')
             ->withTimestamps();
+    }
+
+    public function postViews()
+    {
+        return $this->hasMany(PostView::class);
     }
 }
